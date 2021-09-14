ff3b3757f0539d48183b41018b462c405e4cacfb
vahidGhadiri <whydrf@gmail.com>
2021-09-14 22:20:55 +0430

    Implementing Initial Functionality

--- src/Screens/List/List.component.tsx
+++ src/Screens/List/List.component.tsx
@@ -1,10 +1,35 @@
-import React from "react"
+import React, {ChangeEventHandler, KeyboardEventHandler, useState} from "react"
+
+
+type Task = {
+    label: string,
+}
 
 const List: React.FC = () => {
+    const [task, setTask] = useState<Task[]>([])
+    const [newTaskLabel, setNewTaskLabel] = useState('')
+
+
+    const handleNewTaskLabelChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setNewTaskLabel(e.target.value)
+    }
+
+    const handleNewTaskKeyPress: KeyboardEventHandler<HTMLInputElement> = (e) => {
+        if (e.key === "Enter") setTask([...task, {label: newTaskLabel}])
+    }
+
     return (
-        <div>
-            List
-        </div>
+        <>
+            <ul>
+                {task.map((task, index) => (
+                    <li key={index}>{task.label}</li>
+                ))}
+            </ul>
+            <input value={newTaskLabel}
+                   onChange={(e) => handleNewTaskLabelChange(e)}
+                   onKeyPress={(e) => handleNewTaskKeyPress(e)}
+            />
+        </>
     )
 }
 
